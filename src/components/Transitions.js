67d854d941dc43ee3f4efa09aeeb384864f17b83
dominykasdev <contact@dominykas.dev>
2024-04-24 18:13:26 +0100

    checkpoint

--- src/components/Transitions.js
+++ src/components/Transitions.js
@@ -1,3 +1,4 @@
+import { BsHexagon, BsRecordCircleFill } from "react-icons/bs";
 import { pennRed, kellyGreen, gamboge, moonstone, seasalt } from "../Constants";
 import Stripe from "./Stripe";
 
@@ -10,6 +11,33 @@ const Transitions = ({
 }) => {
   return (
     <div style={{ position: "fixed", width: "100vw", height: "100vh" }}>
+      {/* <BsHexagon
+        size="large"
+        style={{ top: "300px", left: "300px", fill: "#333" }}
+      /> */}
+      <BsRecordCircleFill
+        size="large"
+        style={{ top: "300px", left: "300px", fill: "#333" }}
+      />
+      <Stripe
+        animate={portfolioActive ? "animate" : null}
+        style={{
+          backgroundColor: kellyGreen,
+        }}
+      />
+      <Stripe
+        animate={portfolioActive ? "animate delay" : null}
+        style={{
+          backgroundColor: seasalt,
+        }}
+      />
+      <Stripe
+        animate={portfolioActive ? "animate delay2" : null}
+        style={{
+          backgroundColor: gamboge,
+        }}
+      />
+
       <Stripe
         animate={contactActive ? "animate" : null}
         style={{
