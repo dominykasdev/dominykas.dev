2b26ffa164ae1a986931bc70e14cd80424cd2996
dominykasdev <contact@dominykas.dev>
2024-04-25 09:44:22 +0100

    line transitions animating

--- src/components/LineTransition.js
+++ src/components/LineTransition.js
@@ -8,39 +8,62 @@ const LineTransition = ({ servicesActive }) => {
         position: "fixed",
         width: "100vw",
         height: "100vh",
-        // display: "flex",
-        // justifyContent: "center",
-        // alignItems: "center",
       }}
     >
-      <div className={servicesActive ? "line line1 animate" : "line line1"} />
       <div
-        className={servicesActive ? "line line2 animate delay" : "line line2"}
-      />
-      <div
-        className={servicesActive ? "line line3 animate delay2" : "line line3"}
-      />
-      <div
-        className={servicesActive ? "line line4 animate delay3" : "line line4"}
-      />
-      <div
-        className={servicesActive ? "line line5 animate delay4" : "line line5"}
-      />
-      <div
-        className={servicesActive ? "line line6 animate delay4" : "line line6"}
-      />
-      <div
-        className={servicesActive ? "line line7 animate delay4" : "line line7"}
-      />
-      <div
-        className={servicesActive ? "line line8 animate delay4" : "line line8"}
-      />
-      <div
-        className={servicesActive ? "line line9 animate delay4" : "line line9"}
+        className={
+          servicesActive ? "line line1 left animate" : "line line1 left"
+        }
       />
       <div
         className={
-          servicesActive ? "line line10 animate delay4" : "line line10"
+          servicesActive ? "line line2 right animate delay" : "line line2 right"
+        }
+      />
+      <div
+        className={
+          servicesActive ? "line line3 left animate delay2" : "line line3 left"
+        }
+      />
+      <div
+        className={
+          servicesActive ? "line line4 right animate delay" : "line line4 right"
+        }
+      />
+      <div
+        className={
+          servicesActive ? "line line5 left animate delay1" : "line line5 left"
+        }
+      />
+      <div
+        className={
+          servicesActive
+            ? "line line6 right animate delay3"
+            : "line line6 right"
+        }
+      />
+      <div
+        className={
+          servicesActive ? "line line7 left animate delay4" : "line line7 left"
+        }
+      />
+      <div
+        className={
+          servicesActive
+            ? "line line8 right animate delay3"
+            : "line line8 right"
+        }
+      />
+      <div
+        className={
+          servicesActive ? "line line9 left animate delay1" : "line line9 left"
+        }
+      />
+      <div
+        className={
+          servicesActive
+            ? "line line10 right animate delay2"
+            : "line line10 right"
         }
       />
     </div>
